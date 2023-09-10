7dff98c875f65e8d183f7f076828832891cd0c87
uniblab <uniblab@hotmail.com>
2023-09-09 21:54:49 -0400

    chore: drop custom arg handler for Icod.Argh

--- src/Program.cs
+++ src/Program.cs
@@ -25,34 +25,47 @@ namespace Icod.Clip {
 
 		[System.STAThread]
 		public static System.Int32 Main( System.String[] args ) {
-			if ( null == args ) {
-				args = new System.String[ 0 ];
-			}
 			var len = args.Length;
 			if ( 4 < len ) {
 				PrintUsage();
 				return 1;
 			}
+
+			var processor = new Icod.Argh.Processor(
+				new Icod.Argh.Definition[] {
+					new Icod.Argh.Definition( "help", new System.String[] { "-h", "--help", "/help" } ),
+					new Icod.Argh.Definition( "copyright", new System.String[] { "-c", "--copyright", "/copyright" } ),
+					new Icod.Argh.Definition( "input", new System.String[] { "-i", "--input", "/input" } ),
+					new Icod.Argh.Definition( "output", new System.String[] { "-o", "--output", "/output" } ),
+				},
+				System.StringComparer.OrdinalIgnoreCase
+			);
+			processor.Parse( args );
+
+			if ( processor.Contains( "help" ) ) {
+				PrintUsage();
+				return 1;
+			} else if ( processor.Contains( "copyright" ) ) {
+				PrintCopyright();
+				return 1;
+			}
+
+
 			System.String? inputPathName = null;
+			if ( processor.Contains( "input" ) ) {
+				inputPathName = processor.Value( "input" ).TrimToNull();
+				if ( System.String.IsNullOrEmpty( inputPathName ) ) {
+					PrintUsage();
+					return 1;
+				}
+			}
 			System.String? outputPathName = null;
-			if ( 0 < len ) {
-				--len;
-				System.String @switch;
-				System.Int32 i = -1;
-				do {
-					@switch = args[ ++i ];
-					if ( new System.String[] { "--help", "-h", "/h" }.Contains( @switch, System.StringComparer.OrdinalIgnoreCase ) ) {
-						PrintUsage();
-						return 1;
-					} else if ( new System.String[] { "--copyright", "-c", "/c" }.Contains( @switch, System.StringComparer.OrdinalIgnoreCase ) ) {
-						PrintCopyright();
-						return 1;
-					} else if ( "--input".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-						inputPathName = args[ ++i ].TrimToNull();
-					} else if ( "--output".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-						outputPathName = args[ ++i ].TrimToNull();
-					}
-				} while ( i < len );
+			if ( processor.Contains( "output" ) ) {
+				outputPathName = processor.Value( "output" ).TrimToNull();
+				if ( System.String.IsNullOrEmpty( inputPathName ) ) {
+					PrintUsage();
+					return 1;
+				}
 			}
 
 			System.Func<System.String, System.Collections.Generic.IEnumerable<System.String>> reader;
@@ -99,7 +112,6 @@ namespace Icod.Clip {
 		private static void PrintCopyright() {
 			var copy = new System.String[] {
 				"Clip.exe reads text into the clipboard and outputs same text to either StdOut or a specified file.",
-				"",
 				"Copyright( C ) 2023 Timothy J. Bruce",
 				"",
 				"This program is free software: you can redistribute it and / or modify",
@@ -107,17 +119,12 @@ namespace Icod.Clip {
 				"the Free Software Foundation, either version 3 of the License, or",
 				"( at your option ) any later version.",
 				"",
-				"",
 				"This program is distributed in the hope that it will be useful,",
 				"but WITHOUT ANY WARRANTY; without even the implied warranty of",
-				"",
-				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the",
-				"",
+				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
 				"GNU General Public License for more details.",
 				"",
-				"",
 				"You should have received a copy of the GNU General Public License",
-				"",
 				"along with this program.If not, see < https://www.gnu.org/licenses/>."
 			};
 			foreach ( var line in copy ) {
